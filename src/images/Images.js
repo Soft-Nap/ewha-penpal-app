e3015cacfe2dbfa4af7a70443d347b507d6c479e
Seoyoung Lee <wow9176@naver.com>
2021-08-05 20:06:03 +0900

    Fix : Add comma

--- src/images/Images.js
+++ src/images/Images.js
@@ -3,41 +3,41 @@
 }
 
 const Images = {
-    logo: {
-      uri: require('../images/loginLogo.jpg')
-    },
-    Stamp: {
-      uri: require('../images/Stamp.png')
-    },
-    Bear: {
-      uri: require('../images/bear.png')
-    },
-    Penguin: {
-      uri: require('../images/penguin.png')
-    },
-    Pig: {
-      uri: require('../images/pig.png')
-    },
-    Rabbit: {
-      uri: require('../images/rabbit.png')
-    },
-    MailIcon: {
-      uri: require('../images/mailIcon.png')
-    },
-    E: {
-      uri: require('../images/stampE.png')
-    },
-    W: {
-      uri: require('../images/stampW.png')
-    },
-    H: {
-      uri: require('../images/stampH.png')
-    },
-    A: {
-      uri: require('../images/stampA.png')
-    }
-    HomeLetterEmpty: {
-      uri: require("../images/HomeLetterEmpty.jpg"),
-    },
-}
-  export { Images }; 
+  logo: {
+    uri: require("../images/loginLogo.jpg"),
+  },
+  Stamp: {
+    uri: require("../images/Stamp.png"),
+  },
+  Bear: {
+    uri: require("../images/bear.png"),
+  },
+  Penguin: {
+    uri: require("../images/penguin.png"),
+  },
+  Pig: {
+    uri: require("../images/pig.png"),
+  },
+  Rabbit: {
+    uri: require("../images/rabbit.png"),
+  },
+  MailIcon: {
+    uri: require("../images/mailIcon.png"),
+  },
+  E: {
+    uri: require("../images/stampE.png"),
+  },
+  W: {
+    uri: require("../images/stampW.png"),
+  },
+  H: {
+    uri: require("../images/stampH.png"),
+  },
+  A: {
+    uri: require("../images/stampA.png"),
+  },
+  HomeLetterEmpty: {
+    uri: require("../images/HomeLetterEmpty.jpg"),
+  },
+};
+export { Images };
